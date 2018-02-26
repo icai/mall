381804c35a2ae6b86c77fcddc5ee31cd0b3eec42
root <362463215@qq.com>
2018-02-26 17:03:38 +0800

    年后修改需求后#1

--- core/web/goods/post.php
+++ core/web/goods/post.php
@@ -997,6 +997,152 @@ EOF;
 
 	return $s;
 }
+
+
+function tpl_ueditor1($id, $value = '', $options = array()) {
+	$s = '';
+	if (!defined('TPL_INIT_UEDITOR')) {
+		$s .= '<script type="text/javascript" src="./resource/components/ueditor/ueditor.config.js"></script><script type="text/javascript" src="./resource/components/ueditor/ueditor.all.min.js"></script><script type="text/javascript" src="./resource/components/ueditor/lang/zh-cn/zh-cn.js"></script>';
+	}
+	$options['height'] = empty($options['height']) ? 200 : $options['height'];
+	$options['allow_upload_video'] = isset($options['allow_upload_video']) ? $options['allow_upload_video'] : true;
+	$s .= !empty($id) ? "<textarea id=\"{$id}\" name=\"{$id}\" type=\"text/plain\" style=\"height:{$options['height']}px;\">{$value}</textarea>" : '';
+	$s .= "
+	<script type=\"text/javascript\">
+			var ueditoroption = {
+				'autoClearinitialContent' : false,
+				'toolbars' : [['fullscreen', 'source', 'preview', '|', 'bold', 'italic', 'underline', 'strikethrough', 'forecolor', 'backcolor', '|',
+					'justifyleft', 'justifycenter', 'justifyright', '|', 'insertorderedlist', 'insertunorderedlist', 'blockquote', 'emotion',
+					'link', 'removeformat', '|', 'rowspacingtop', 'rowspacingbottom', 'lineheight','indent', 'paragraph', 'fontsize', '|',
+					'inserttable', 'deletetable', 'insertparagraphbeforetable', 'insertrow', 'deleterow', 'insertcol', 'deletecol',
+					'mergecells', 'mergeright', 'mergedown', 'splittocells', 'splittorows', 'splittocols', '|', 'anchor', 'map', 'print', 'drafts']],
+				'elementPathEnabled' : false,
+				'initialFrameHeight': {$options['height']},
+				'focus' : false,
+				'maximumWords' : 9999999999999
+			};
+			var opts = {
+				type :'image',
+				direct : false,
+				multiple : true,
+				tabs : {
+					'upload' : 'active',
+					'browser' : '',
+					'crawler' : ''
+				},
+				path : '',
+				dest_dir : '',
+				global : false,
+				thumb : false,
+				width : 0
+			};
+			UE.registerUI('myinsertimage',function(editor,uiName){
+				editor.registerCommand(uiName, {
+					execCommand:function(){
+						require(['fileUploader'], function(uploader){
+							uploader.show(function(imgs){
+								if (imgs.length == 0) {
+									return;
+								} else if (imgs.length == 1) {
+									editor.execCommand('insertimage', {
+										'src' : imgs[0]['url'],
+										'_src' : imgs[0]['attachment'],
+										'width' : '100%',
+										'alt' : imgs[0].filename
+									});
+								} else {
+									var imglist = [];
+									for (i in imgs) {
+										imglist.push({
+											'src' : imgs[i]['url'],
+											'_src' : imgs[i]['attachment'],
+											'width' : '100%',
+											'alt' : imgs[i].filename
+										});
+									}
+									editor.execCommand('insertimage', imglist);
+								}
+							}, opts);
+						});
+					}
+				});
+				var btn = new UE.ui.Button({
+					name: '插入图片',
+					title: '插入图片',
+					cssRules :'background-position: -726px -77px',
+					onclick:function () {
+						editor.execCommand(uiName);
+					}
+				});
+				editor.addListener('selectionchange', function () {
+					var state = editor.queryCommandState(uiName);
+					if (state == -1) {
+						btn.setDisabled(true);
+						btn.setChecked(false);
+					} else {
+						btn.setDisabled(false);
+						btn.setChecked(state);
+					}
+				});
+				return btn;
+			}, 19);
+			UE.registerUI('myinsertvideo',function(editor,uiName){
+				editor.registerCommand(uiName, {
+					execCommand:function(){
+						require(['fileUploader'], function(uploader){
+							uploader.show(function(video){
+								if (!video) {
+									return;
+								} else {
+									var videoType = video.isRemote ? 'iframe' : 'video';
+									editor.execCommand('insertvideo', {
+										'url' : video.url,
+										'width' : 300,
+										'height' : 200
+									}, videoType);
+								}
+							}, {type : 'video', allowUploadVideo : ".($options['allow_upload_video'] ? 'true' : 'false')."});
+						});
+					}
+				});
+				var btn = new UE.ui.Button({
+					name: '插入视频',
+					title: '插入视频',
+					cssRules :'background-position: -320px -20px',
+					onclick:function () {
+						editor.execCommand(uiName);
+					}
+				});
+				editor.addListener('selectionchange', function () {
+					var state = editor.queryCommandState(uiName);
+					if (state == -1) {
+						btn.setDisabled(true);
+						btn.setChecked(false);
+					} else {
+						btn.setDisabled(false);
+						btn.setChecked(state);
+					}
+				});
+				return btn;
+			}, 20);
+			".(!empty($id) ? "
+				$(function(){
+					var ue = UE.getEditor('{$id}', ueditoroption);
+					$('#{$id}').data('editor', ue);
+					$('#{$id}').parents('form').submit(function() {
+						if (ue.queryCommandState('source')) {
+							ue.execCommand('source');
+						}
+					});
+				});" : '')."
+	</script>";
+	return $s;
+}
+
+
+
+
+
 include $this->template('goods/post');
 exit();
 ?>
